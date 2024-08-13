caf635ffd296af53a7c2c5eab5ad953e4958df51
Tom Lehman <tom.s.lehman@gmail.com>
2024-08-13 10:06:09 -0400

    Fix migration

--- db/migrate/20240812202530_remove_not_null_constraint_from_facet_transactions.rb
+++ db/migrate/20240812202530_remove_not_null_constraint_from_facet_transactions.rb
@@ -5,7 +5,7 @@ class RemoveNotNullConstraintFromFacetTransactions < ActiveRecord::Migration[6.1
   end
 
   def down
-    change_column_null :facet_transactions, :eth_transaction_hash, false
-    change_column_null :facet_transactions, :eth_call_index, false
+    # change_column_null :facet_transactions, :eth_transaction_hash, false
+    # change_column_null :facet_transactions, :eth_call_index, false
   end
 end
