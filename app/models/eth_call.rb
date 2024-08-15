e2480fadd75b546625afd647d93e55789286109c
Tom Lehman <tom.s.lehman@gmail.com>
2024-08-15 08:17:16 -0400

    Improve eth call parsing efficiency

--- app/models/eth_call.rb
+++ app/models/eth_call.rb
@@ -13,9 +13,9 @@ class EthCall < ApplicationRecord
   def self.from_trace_result(trace_result, eth_block)
     order_counter = Struct.new(:count).new(0)
     
-    trace_result.map do |trace|
-      process_trace(trace, eth_block, order_counter)
-    end.flatten
+    trace_result.each_with_object([]) do |trace, traces|
+      traces.concat(process_trace(trace, eth_block, order_counter))
+    end
   end
 
   private
@@ -26,35 +26,38 @@ class EthCall < ApplicationRecord
     order_counter.count += 1
 
     traces = []
-    
-    traced_call = EthCall.new(
-      block_hash: eth_block.block_hash,
-      block_number: eth_block.number,
-      transaction_hash: trace['txHash'],
-      from_address: result['from'],
-      to_address: result['to'],
-      gas: result['gas'].to_i(16),
-      gas_used: result['gasUsed'].to_i(16),
-      input: result['input'],
-      output: result['output'],
-      value: result['value'],
-      call_type: result['type'],
-      error: result['error'],
-      revert_reason: result['revertReason'],
-      call_index: current_order,
-      parent_call_index: parent_traced_call&.call_index
-    )
-    
-    traces << traced_call
-     
+    # Check if the current call is relevant
+    if result['to'] == FacetTransaction::FACET_INBOX_ADDRESS && result['error'].nil?
+      traced_call = EthCall.new(
+        block_hash: eth_block.block_hash,
+        block_number: eth_block.number,
+        transaction_hash: trace['txHash'],
+        from_address: result['from'],
+        to_address: result['to'],
+        gas: result['gas'].to_i(16),
+        gas_used: result['gasUsed'].to_i(16),
+        input: result['input'],
+        output: result['output'],
+        value: result['value'],
+        call_type: result['type'],
+        error: result['error'],
+        revert_reason: result['revertReason'],
+        call_index: current_order,
+        parent_call_index: parent_traced_call&.call_index
+      )
+      
+      traces << traced_call
+    end
+  
+    # Always process sub-calls
     if result['calls']
-      traces += result['calls'].map do |sub_call|
-        process_trace(
+      result['calls'].each do |sub_call|
+        traces.concat(process_trace(
           { 'txHash' => trace['txHash'], 'result' => sub_call },
           eth_block,
           order_counter,
           traced_call
-        )
+        ))
       end
     end
     
