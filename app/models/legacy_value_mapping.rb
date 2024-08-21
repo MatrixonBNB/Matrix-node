3b4ae9db6ba09a99eae0f9a55a2c0e9e13e9d497
Tom Lehman <tom.s.lehman@gmail.com>
2024-08-21 17:16:29 -0400

    Mappings writer

--- app/models/legacy_value_mapping.rb
+++ app/models/legacy_value_mapping.rb
@@ -1,3 +1,8 @@
 class LegacyValueMapping < ApplicationRecord
   validates :mapping_type, :legacy_value, :new_value, presence: true
+  
+  def self.to_file(filename)
+    json = JSON.pretty_generate(LegacyValueMapping.all.map(&:as_json))
+    File.write(filename, json)
+  end
 end
