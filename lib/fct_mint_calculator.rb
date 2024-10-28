088348c47812eed0e42cf258e37f9cd619ba990b
Tom Lehman <tom.s.lehman@gmail.com>
2024-10-28 11:54:45 -0400

    Fix fct mint rate

--- lib/fct_mint_calculator.rb
+++ lib/fct_mint_calculator.rb
@@ -35,15 +35,15 @@ module FctMintCalculator
     TARGET_MINT_PER_PERIOD / halving_factor(l2_block)
   end
 
-  def compute_new_rate(facet_block, prev_rate, cumulative_mint_in_period)
+  def compute_new_rate(facet_block, prev_rate, cumulative_l1_data_gas)
     if is_first_block_in_period?(facet_block)
-      if cumulative_mint_in_period == 0
+      if cumulative_l1_data_gas == 0
         new_rate = MAX_RATE
       else
         halving_adjusted_target = halving_adjusted_target(facet_block)
         return 0 if halving_adjusted_target == 0
         
-        new_rate = halving_adjusted_target / cumulative_mint_in_period
+        new_rate = halving_adjusted_target / cumulative_l1_data_gas
       end
 
       max_allowed_rate = [prev_rate * MAX_ADJUSTMENT_FACTOR, MAX_RATE].min
@@ -64,7 +64,7 @@ module FctMintCalculator
       
       facet_block.assign_attributes(
         fct_mint_rate: BASE_RATE,
-        fct_minted_in_rate_adjustment_period: 0
+        fct_mint_period_l1_data_gas: 0
       )
       
       return
@@ -72,23 +72,25 @@ module FctMintCalculator
     
     prev_l1_attributes = GethDriver.client.get_l1_attributes(facet_block.number - 1)
     prev_rate = prev_l1_attributes[:fct_mint_rate] 
-    cumulative_mint_in_period = prev_l1_attributes[:fct_minted_in_rate_adjustment_period]
+    cumulative_l1_data_gas = prev_l1_attributes[:fct_mint_period_l1_data_gas]
     
-    new_rate = compute_new_rate(facet_block, prev_rate, cumulative_mint_in_period)
+    new_rate = compute_new_rate(facet_block, prev_rate, cumulative_l1_data_gas)
     
     facet_txs.each do |tx|
-      tx.mint = tx.l1_calldata_gas_used * new_rate
+      tx.mint = tx.l1_data_gas_used * new_rate
     end
     
+    batch_l1_data_gas = facet_txs.sum(&:l1_data_gas_used)
+    
     if is_first_block_in_period?(facet_block)
-      new_cumulative_mint = facet_txs.sum(&:mint)
+      new_cumulative_l1_data_gas = batch_l1_data_gas
     else
-      new_cumulative_mint = cumulative_mint_in_period + facet_txs.sum(&:mint)
+      new_cumulative_l1_data_gas = cumulative_l1_data_gas + batch_l1_data_gas
     end
     
     facet_block.assign_attributes(
       fct_mint_rate: new_rate,
-      fct_minted_in_rate_adjustment_period: new_cumulative_mint
+      fct_mint_period_l1_data_gas: new_cumulative_l1_data_gas
     )
     
     nil
